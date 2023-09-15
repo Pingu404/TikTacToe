1bc3d0750aec6d27ab991dc466a535d879ec6b32
Andrew <ajdwashburn@gmail.com>
2023-09-15 10:42:43 -0700

    Initial commit

---
+++ src/TikTacToe.java
@@ -0,0 +1,249 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+	
+public class TikTacToe {
+
+	TikTacToe() {
+	/*	
+		a  b   c
+	1	__|__|__
+	2	__|__|__
+	3	__|__|__
+	
+	
+	winners: a1 a2 a3, b1 b2 b3, c1 c2 c3, a1 b1 c1, a2 b2 c2, a3 b3 c3, a1 b2 c3, c1 b2 a1,
+	*/
+	
+		
+		
+	String a1 = "__";
+	String b1 = "__";
+	String c1 = "__";
+	String a2 = "__";
+	String b2 = "__";
+	String c2 = "__";
+	String a3 = "__";
+	String b3 = "__";
+	String c3 = "__";
+	int turns = 1;
+	int movesSoFar = 0;
+	
+	//the orignal bord
+	System.out.println("  a  b  c");
+	System.out.println("1 "+a1+"|"+b1+"|"+c1);
+	System.out.println("2 "+a2+"|"+b2+"|"+c2);
+	System.out.println("3 "+a3+"|"+b3+"|"+c3);
+		
+		System.out.println("Welcome to TikTacToe!!!");
+		System.out.println("please input the move for x to begin");
+		
+		
+		
+		do {
+			
+		
+			
+		
+			Scanner scanner2 = new Scanner(System.in);
+			String move = scanner2.nextLine();
+			//Sets the value for the move
+			if (move.equals("a1")) {
+		    a1 = "X "; 
+			}
+			if (move.equals("a2")) {
+			    a2 = "X "; 
+				}
+			if (move.equals("a3")) {
+				    a3 = "X "; 
+				}
+			if (move.equals("b1")) {
+			    b1 = "X "; 
+				}
+			if (move.equals("b2")) {
+			    b2 = "X "; 
+				}
+			if (move.equals("b3")) {
+			    b3 = "X "; 
+				}
+			if (move.equals("c1")) {
+			    c1 = "X "; 
+				}
+			if (move.equals("c2")) {
+			    c2 = "X "; 
+				}
+			if (move.equals("c3")) {
+			    c3 = "X "; 
+				}
+			
+			//prints out the new bord
+			System.out.println("  a  b  c");
+			System.out.println("1 "+a1+"|"+b1+"|"+c1);
+			System.out.println("2 "+a2+"|"+b2+"|"+c2);
+			System.out.println("3 "+a3+"|"+b3+"|"+c3);
+			 
+			if((a1.equals("X "))&&(a2.equals("X "))&&(a3.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			
+			if((b1.equals("X "))&&(b2.equals("X "))&&(b3.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((c1.equals("X "))&&(c2.equals("X "))&&(c3.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((a1.equals("X "))&&(b1.equals("X "))&&(c1.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((a2.equals("X "))&&(b2.equals("X "))&&(c2.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((a3.equals("X "))&&(b3.equals("X "))&&(c3.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((a1.equals("X "))&&(b2.equals("X "))&&(c3.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			if((a3.equals("X "))&&(b2.equals("X "))&&(c1.equals("X ")))
+			{
+				System.out.println("X WINS");
+				turns++;
+			
+			}
+			
+			if (movesSoFar == 9)
+			{
+				System.out.println("The game is a tie!!!");
+				turns++;
+			}
+						
+			
+				Scanner scanner = new Scanner(System.in);
+				String move0 = scanner.nextLine();
+				//Sets the value for the move
+				
+				if (move0.equals("a1")) {
+			    a1 = "0 "; 
+				}
+				if (move0.equals("a2")) {
+				    a2 = "0 "; 
+					}
+				if (move0.equals("a3")) {
+					    a3 = "0 "; 
+					}
+				if (move0.equals("b1")) {
+				    b1 = "0 "; 
+					}
+				if (move0.equals("b2")) {
+				    b2 = "0 "; 
+					}
+				if (move0.equals("b3")) {
+				    b3 = "0 "; 
+					}
+				if (move0.equals("c1")) {
+				    c1 = "0 "; 
+					}
+				if (move0.equals("c2")) {
+				    c2 = "0 "; 
+					}
+				if (move0.equals("c3")) {
+				    c3 = "0 "; 
+					}
+				
+				//prints out the new bord
+				System.out.println("  a  b  c");
+				System.out.println("1 "+a1+"|"+b1+"|"+c1);
+				System.out.println("2 "+a2+"|"+b2+"|"+c2);
+				System.out.println("3 "+a3+"|"+b3+"|"+c3);
+				
+				
+				if((a1.equals("0 "))&&(a2.equals("0 "))&&(a3.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				
+				if((b1.equals("0 "))&&(b2.equals("0 "))&&(b3.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				}
+				if((c1.equals("0 "))&&(c2.equals("0 "))&&(c3.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				if((a1.equals("0 "))&&(b1.equals("0 "))&&(c1.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				if((a2.equals("0 "))&&(b2.equals("0 "))&&(c2.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				if((a3.equals("0 "))&&(b3.equals("0 "))&&(c3.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				if((a1.equals("0 "))&&(b2.equals("0 "))&&(c3.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				if((a3.equals("0 "))&&(b2.equals("0 "))&&(c1.equals("0 ")))
+				{
+					System.out.println("0 WINS");
+					turns++;
+				
+				}
+				
+				
+		} while (turns < 2);
+		System.out.println("Would you like to play again? y/n");
+		
+		Scanner scanner3 = new Scanner(System.in);
+		String play = scanner3.nextLine();
+		 if (play.equals(y))
+		 {
+			 new main();
+		 }
+	}
+		
+	
+	public static void main(String[] args) {
+		int 
+new TikTacToe();
+
+
+	}
+
+}
