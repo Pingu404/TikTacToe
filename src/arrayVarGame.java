cec7e511b045e7e1601b0ceaaaa048187184b78c
gooey123 <ajdwashburn@gmail.com>
2023-10-10 20:28:54 -0700

    Here we go

--- src/arrayVarGame.java
+++ src/arrayVarGame.java
@@ -1,72 +1,73 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
-import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class arrayVarGame {
-
-	arrayVarGame() {
 	
-		//The rows
-		String[] row1 = { "__", "__", "__"};
-		String[] row2 = { "__", "__", "__"};
-		String[] row3 = { "__", "__", "__"};
-		int turns = 0;
-		int xoro = 0;
-		//prints the actual code
-		System.out.println("  0  1  2");
-		System.out.println("1 "+row1[0]+"|"+row1[1]+"|"+row1[2]);
-		System.out.println("2 "+row2[0]+"|"+row2[1]+"|"+row2[2]);
-		System.out.println("3 "+row3[0]+"|"+row3[1]+"|"+row3[2]);
+	public String[] row1 = { "__", "__", "__"};
+	public String[] row2 = { "__", "__", "__"};
+	public String[] row3 = { "__", "__", "__"};
+	public int turns = 0;
+	public int xoro = 0;  
+	//lol
+	arrayVarGame() {
+		JFrame frame = new JFrame("Tic Tac Toe");
 		
+	
 		
-	//I orignaly made it without using rows or arrays so i am making this one now. lol
-		System.out.println("Welcome to the version of tiktactoe I was supposed to make.");
-		-
-		
-		
-		
-		do {
-			if (xoro == 0)
-			{
-				System.out.println("Please enter the row");
-				Scanner scanner1 = new Scanner(System.in);
-				String rowX = scanner1.nextLine();
-				
-				
-				System.out.println("what is the colloum");
-				Scanner scanner1 = new Scanner(System.in);
-				String input1 = scanner1.nextLine();
-				int  colX = Integer.parseInt(input1);
-				
-				if (rowX = "1" || row1[colX] = "__") {
-					A[colX] = "X ";
-				} else if (row1[colX] = "__" && (rowX.equals("2"))) {
-					row2[colX] = "X ";
-				} else if (rowX == 3) {
-					row3[colX] = "X ";
-				} else
-				
-				System.out.println("  0  1  2");
-				System.out.println("1 "+row1[0]+"|"+row1[1]+"|"+row1[2]);
-				System.out.println("2 "+row2[0]+"|"+row2[1]+"|"+row2[2]);
-				System.out.println("3 "+row3[0]+"|"+row3[1]+"|"+row3[2]);
-			 xoro++;
-			}
-			if (xoro == 1) {
-				
-			}
+		GridLayout grid = new GridLayout(3, 3);
+
+	        frame.setVisible(true);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setSize(600, 600);
+	        frame.setLayout(grid);
+	        frame.setResizable(false);
+
+	        JButton button1 = new JButton(row1[0]);
+	        JButton button2 = new JButton(row1[1]);
+	        JButton button3 = new JButton(row1[2]);
+	        JButton button4 = new JButton(row2[0]);
+	        JButton button5 = new JButton(row2[1]);
+	        JButton button6 = new JButton(row2[2]);
+	        JButton button7 = new JButton(row3[0]);
+	        JButton button8 = new JButton(row3[1]);
+	        JButton button9 = new JButton(row3[2]);
+
+	        frame.add(button1);
+	        frame.add(button2);
+	        frame.add(button3);
+	        frame.add(button4);
+	        frame.add(button5);
+	        frame.add(button6);
+	        frame.add(button7);
+	        frame.add(button8);
+	        frame.add(button9);
+	        
+	    //add all of the other buttons    
+	 button1.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent evt) {
+             // delegate to event handler method
+            if (row1[0].equals("__")) {
+        	 if (xoro == 0) { 
+            		row1[0] = "X ";
+            		xoro++;
+            	 } else if (xoro == 1) {
+            		 row1[0] = "0 ";
+            		 xoro--; } } else { System.out.println("Invalid Move"); } } });
+}
+	
+	
+	public void winCheck() {
+		//Add all of the other possable winns
+		if (row1[0].equals(row1[1].equals(row1[2]))&&(!row1[0].equals("__"))) {
 			
-		Scanner scanner2 = new Scanner(System.in);
-		String move = scanner2.nextLine();
-		} while (turns < 9);
+		}
+		
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
+	
